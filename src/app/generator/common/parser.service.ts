4bf707fea53413a12e5750bd2950584bff4d5a1c
P-WM <P-WM@users.noreply.github.com>
2017-10-10 20:17:02 +0100

    Implement ParserService Need to set types better on test objects

--- src/app/generator/common/parser.service.ts
+++ src/app/generator/common/parser.service.ts
@@ -10,16 +10,41 @@ export class ParserService {
     this._grammar = grammar;
   }
 
-  public parseSmut(str: string, sentence=true): Token {
-    const root: Token = new Token(TokenType.SENTENCE);
+  private _getFromGrammar(token: string): string {
+    const tokens = this._grammar[token];
+    return tokens[Math.floor(Math.random() * tokens.length)];
+  }
 
-    // Iterate through str, adding correct components to root IToken
+  private _tokeniseText(str: string): Array<Token> {
+    const tokens: Array<Token> = [];
+    let chunk: string = '';
+
+    // Iterate through str, creating appropriate tokens and adding to return array
     for (const char of str) {
       if (/[\[<]/.test(char)) {
-
+        if (chunk) {
+          tokens.push(new Token(TokenType.TEXT, chunk));
+          chunk = '';
+        }
+      } else if (/[\]>]/.test(char)) {
+        const newToken = new Token(TokenType.BNF, chunk, char == ']');
+        tokens.push(newToken);
+        if (!newToken.bnfTerminal) {
+          newToken.components = this._tokeniseText(this._getFromGrammar(chunk));
+        }
+        chunk = '';
+      } else {
+        chunk += char;
       }
     }
+    chunk && tokens.push(new Token(TokenType.TEXT, chunk));
+    return tokens;
+  }
 
+  public parseSmut(str: string): Token {
+    const root: Token = new Token(TokenType.SENTENCE);
+    root.components = this._tokeniseText(str);
     return root;
   }
+
 }
