4ed27f64ca2ddda3f94bee0ab5ccb67fa044fbf8
P-WM <P-WM@users.noreply.github.com>
2017-10-10 21:56:09 +0100

    Improve style of Token constructor

--- src/app/generator/common/token.model.ts
+++ src/app/generator/common/token.model.ts
@@ -1,18 +1,7 @@
 export enum TokenType { 'SENTENCE', 'TEXT', 'BNF' }
 
 export class Token {
-  public type: TokenType;
-  public value?: string;
-  public bnfTerminal?: boolean;
   public components: Array<Token>;
 
-  constructor(type: TokenType, value?: string, bnfTerminal?: boolean) {
-    this.type = type;
-    if (value !== undefined) {
-      this.value = value;
-    }
-    if (bnfTerminal !== undefined) {
-      this.bnfTerminal = bnfTerminal;
-    }
-  }
+  constructor(public type: TokenType, public value?: string, public bnfTerminal?: boolean) {}
 }
