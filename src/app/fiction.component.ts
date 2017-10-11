bb4db4be3b4f6d382adc6f7606e65ce90e14b8ae
P-WM <P-WM@users.noreply.github.com>
2017-10-11 20:56:51 +0100

    Add button to produce new erotic fiction

--- src/app/fiction.component.ts
+++ src/app/fiction.component.ts
@@ -4,12 +4,19 @@ import { GeneratorService } from './generator';
 
 @Component({
   selector: 'erotic-fiction',
-  template: '<div id="fiction">{{smut}}</div>'
+  template: `
+    <div id="fiction">{{smut}}</div>
+    <button (click)="_updateSmut()">More please</button>
+  `
 })
 export class FictionComponent {
   public smut: string;
 
   constructor(private _fictionGenerator: GeneratorService) {
-    this.smut = _fictionGenerator.getSmut();
+    this._updateSmut();
+  }
+
+  private _updateSmut() {
+    this.smut = this._fictionGenerator.getSmut();
   }
 };
